79a0b7c4120378cda676aa7380450f96688f1bc7
kawano-y <kawano.3c9e@gmail.com>
2021-08-29 23:48:00 +0900

    fix

---
+++ src/main/java/jp/k/englishsearch/EnglishSearchApplication.java
@@ -0,0 +1,13 @@
+package jp.k.englishsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/k/englishsearch/EnglishSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.k.englishsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
